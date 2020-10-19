34172487877d46af55ac9b2b34c29da2702c233e
AnkitYande <ankityande@gmail.com>
2020-10-18 19:01:46 -0500

    svg animation

--- animate.js
+++ animate.js
@@ -1,24 +1,185 @@
 const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
+const myimg = document.querySelector("img");
 
 var controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
-    triggerElement: intro, 
-    triggerHook: 0,
-    duration: 2000
-})
+// let scrollpos = 0;
+
+// scene.on("update", e=>{
+//     scrollpos = e.scrollPos/1000;
+// })
+
+// setInterval(() => {
+//     video.currentTime = scrollpos;
+//     $("#img").attr("src", imgs[obj.curImg]);
+// }, 60);
+
+// var scene = new ScrollMagic.Scene({
+//     triggerElement: intro, 
+//     triggerHook: 0,
+//     duration: 2000
+// })
+//     .addIndicators() // add indicators (requires plugin)
+//     .setPin(intro)
+//     .setTween(tween)
+//     .addTo(controller);
+
+var images = [
+    "./AY_svg_seq/AY_svg_seq0001.svg",
+    "./AY_svg_seq/AY_svg_seq0002.svg",
+    "./AY_svg_seq/AY_svg_seq0003.svg",
+    "./AY_svg_seq/AY_svg_seq0004.svg",
+    "./AY_svg_seq/AY_svg_seq0005.svg",
+    "./AY_svg_seq/AY_svg_seq0006.svg",
+    "./AY_svg_seq/AY_svg_seq0007.svg",
+    "./AY_svg_seq/AY_svg_seq0008.svg",
+    "./AY_svg_seq/AY_svg_seq0009.svg",
+    "./AY_svg_seq/AY_svg_seq0010.svg",
+    "./AY_svg_seq/AY_svg_seq0011.svg",
+    "./AY_svg_seq/AY_svg_seq0012.svg",
+    "./AY_svg_seq/AY_svg_seq0013.svg",
+    "./AY_svg_seq/AY_svg_seq0014.svg",
+    "./AY_svg_seq/AY_svg_seq0015.svg",
+    "./AY_svg_seq/AY_svg_seq0016.svg",
+    "./AY_svg_seq/AY_svg_seq0017.svg",
+    "./AY_svg_seq/AY_svg_seq0018.svg",
+    "./AY_svg_seq/AY_svg_seq0019.svg",
+    "./AY_svg_seq/AY_svg_seq0020.svg",
+    "./AY_svg_seq/AY_svg_seq0021.svg",
+    "./AY_svg_seq/AY_svg_seq0022.svg",
+    "./AY_svg_seq/AY_svg_seq0023.svg",
+    "./AY_svg_seq/AY_svg_seq0024.svg",
+    "./AY_svg_seq/AY_svg_seq0025.svg",
+    "./AY_svg_seq/AY_svg_seq0026.svg",
+    "./AY_svg_seq/AY_svg_seq0027.svg",
+    "./AY_svg_seq/AY_svg_seq0028.svg",
+    "./AY_svg_seq/AY_svg_seq0029.svg",
+    "./AY_svg_seq/AY_svg_seq0030.svg",
+    "./AY_svg_seq/AY_svg_seq0031.svg",
+    "./AY_svg_seq/AY_svg_seq0032.svg",
+    "./AY_svg_seq/AY_svg_seq0033.svg",
+    "./AY_svg_seq/AY_svg_seq0034.svg",
+    "./AY_svg_seq/AY_svg_seq0035.svg",
+    "./AY_svg_seq/AY_svg_seq0036.svg",
+    "./AY_svg_seq/AY_svg_seq0037.svg",
+    "./AY_svg_seq/AY_svg_seq0038.svg",
+    "./AY_svg_seq/AY_svg_seq0039.svg",
+    "./AY_svg_seq/AY_svg_seq0040.svg",
+    "./AY_svg_seq/AY_svg_seq0041.svg",
+    "./AY_svg_seq/AY_svg_seq0042.svg",
+    "./AY_svg_seq/AY_svg_seq0043.svg",
+    "./AY_svg_seq/AY_svg_seq0044.svg",
+    "./AY_svg_seq/AY_svg_seq0045.svg",
+    "./AY_svg_seq/AY_svg_seq0046.svg",
+    "./AY_svg_seq/AY_svg_seq0047.svg",
+    "./AY_svg_seq/AY_svg_seq0048.svg",
+    "./AY_svg_seq/AY_svg_seq0049.svg",
+    "./AY_svg_seq/AY_svg_seq0050.svg",
+    "./AY_svg_seq/AY_svg_seq0051.svg",
+    "./AY_svg_seq/AY_svg_seq0052.svg",
+    "./AY_svg_seq/AY_svg_seq0053.svg",
+    "./AY_svg_seq/AY_svg_seq0054.svg",
+    "./AY_svg_seq/AY_svg_seq0055.svg",
+    "./AY_svg_seq/AY_svg_seq0056.svg",
+    "./AY_svg_seq/AY_svg_seq0057.svg",
+    "./AY_svg_seq/AY_svg_seq0058.svg",
+    "./AY_svg_seq/AY_svg_seq0059.svg",
+    "./AY_svg_seq/AY_svg_seq0060.svg",
+    "./AY_svg_seq/AY_svg_seq0061.svg",
+    "./AY_svg_seq/AY_svg_seq0062.svg",
+    "./AY_svg_seq/AY_svg_seq0063.svg",
+    "./AY_svg_seq/AY_svg_seq0064.svg",
+    "./AY_svg_seq/AY_svg_seq0065.svg",
+    "./AY_svg_seq/AY_svg_seq0066.svg",
+    "./AY_svg_seq/AY_svg_seq0067.svg",
+    "./AY_svg_seq/AY_svg_seq0068.svg",
+    "./AY_svg_seq/AY_svg_seq0069.svg",
+    "./AY_svg_seq/AY_svg_seq0070.svg",
+    "./AY_svg_seq/AY_svg_seq0071.svg",
+    "./AY_svg_seq/AY_svg_seq0072.svg",
+    "./AY_svg_seq/AY_svg_seq0073.svg",
+    "./AY_svg_seq/AY_svg_seq0074.svg",
+    "./AY_svg_seq/AY_svg_seq0075.svg",
+    "./AY_svg_seq/AY_svg_seq0076.svg",
+    "./AY_svg_seq/AY_svg_seq0077.svg",
+    "./AY_svg_seq/AY_svg_seq0078.svg",
+    "./AY_svg_seq/AY_svg_seq0079.svg",
+    "./AY_svg_seq/AY_svg_seq0080.svg",
+    "./AY_svg_seq/AY_svg_seq0081.svg",
+    "./AY_svg_seq/AY_svg_seq0082.svg",
+    "./AY_svg_seq/AY_svg_seq0083.svg",
+    "./AY_svg_seq/AY_svg_seq0084.svg",
+    "./AY_svg_seq/AY_svg_seq0085.svg",
+    "./AY_svg_seq/AY_svg_seq0086.svg",
+    "./AY_svg_seq/AY_svg_seq0087.svg",
+    "./AY_svg_seq/AY_svg_seq0088.svg",
+    "./AY_svg_seq/AY_svg_seq0089.svg",
+    "./AY_svg_seq/AY_svg_seq0090.svg",
+    "./AY_svg_seq/AY_svg_seq0091.svg",
+    "./AY_svg_seq/AY_svg_seq0092.svg",
+    "./AY_svg_seq/AY_svg_seq0093.svg",
+    "./AY_svg_seq/AY_svg_seq0094.svg",
+    "./AY_svg_seq/AY_svg_seq0095.svg",
+    "./AY_svg_seq/AY_svg_seq0096.svg",
+    "./AY_svg_seq/AY_svg_seq0097.svg",
+    "./AY_svg_seq/AY_svg_seq0098.svg",
+    "./AY_svg_seq/AY_svg_seq0099.svg",
+    "./AY_svg_seq/AY_svg_seq0100.svg",
+    "./AY_svg_seq/AY_svg_seq0101.svg",
+    "./AY_svg_seq/AY_svg_seq0102.svg",
+    "./AY_svg_seq/AY_svg_seq0103.svg",
+    "./AY_svg_seq/AY_svg_seq0104.svg",
+    "./AY_svg_seq/AY_svg_seq0105.svg",
+    "./AY_svg_seq/AY_svg_seq0106.svg",
+    "./AY_svg_seq/AY_svg_seq0107.svg",
+    "./AY_svg_seq/AY_svg_seq0108.svg",
+    "./AY_svg_seq/AY_svg_seq0109.svg",
+    "./AY_svg_seq/AY_svg_seq0110.svg",
+    "./AY_svg_seq/AY_svg_seq0111.svg",
+    "./AY_svg_seq/AY_svg_seq0112.svg",
+    "./AY_svg_seq/AY_svg_seq0113.svg",
+    "./AY_svg_seq/AY_svg_seq0114.svg",
+    "./AY_svg_seq/AY_svg_seq0115.svg",
+    "./AY_svg_seq/AY_svg_seq0116.svg",
+    "./AY_svg_seq/AY_svg_seq0117.svg",
+    "./AY_svg_seq/AY_svg_seq0118.svg",
+    "./AY_svg_seq/AY_svg_seq0119.svg",
+    "./AY_svg_seq/AY_svg_seq0120.svg"
+];
+
+var obj = {curImg: 0};
+
+	// create tween
+// var tween = TweenMax.to(obj, 0.5,
+//     {
+//         curImg: images.length - 1,	// animate propery curImg to number of images
+//         roundProps: "curImg",				// only integers so it can be used as an array index
+//         repeat: 3,									// repeat 3 times
+//         immediateRender: true,			// load first image automatically
+//         ease: Linear.easeNone,			// show every image the same ammount of time
+//         onUpdate: function () {
+//             myimg.src=images[obj.curImg]); // set the image source
+//         }
+//     }
+// );
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: intro, triggerHook: 0, duration: 1700})
     .addIndicators() // add indicators (requires plugin)
     .setPin(intro)
     .addTo(controller);
 
 let scrollpos = 0;
 
-    
 scene.on("update", e=>{
-    scrollpos = e.scrollPos/1000;
-})
+    scrollpos = e.scrollPos*0.06;
+    scrollpos = scrollpos > 119 ? 119 : Math.round(scrollpos);
+    console.log(scrollpos);
+});
 
 setInterval(() => {
-    video.currentTime = scrollpos;
-}, 60);
+    myimg.src = images[scrollpos]
+},0.5);
