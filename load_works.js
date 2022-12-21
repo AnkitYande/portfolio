1d3b8c46876db6a0301e1e99626daf166890e305
Ankit Yande <ankityande@gmail.com>
2022-12-21 17:47:26 -0600

    changed work section

--- load_works.js
+++ load_works.js
@@ -1,73 +1,82 @@
 const works = {
-    "websites": [
+    "work": [
         {
             "text": "This Portfolio!",
             "imageSrc": "./assets/Web Development/portfolio.png",
-            "onClick": "./web-dev/MyPortfolio.html",
+            "onClick": "./work/MyPortfolio.html",
+            "tags": ["website", "frontend", "design"]
         },
         {
             "text": "MERN Stack Twitter Clone",
             "imageSrc": "./assets/Web Development/TwitterClone3.png",
-            "onClick": "./web-dev/TwitterClone.html",
+            "onClick": "./work/TwitterClone.html",
+            "tags": ["website", "fullstack"]
         },
         {
             "text": "Collaborative Socket Whiteboard",
             "imageSrc": "./assets/Web Development/draw.png",
             "onClick": "https://github.com/AnkitYande/socket-paint-client",
+            "tags": ["website", "frontend", "fullstack"]
         },
         {
             "text": "Flutter Spotify UI Clone",
             "imageSrc": "./assets/Web Development/SpotifyCloneMockup.png",
             "onClick": "https://github.com/AnkitYande/SpotifyUIClone",
+            "tags": ["frontend"]
         },
         {
             "text": "Interactive Spotify Player",
             "imageSrc": "./assets/Web Development/SpotifyPlayer2.png",
             "onClick": "https://github.com/AnkitYande/ReactSpotifyPlayer",
+            "tags": ["website", "frontend", "fullstack"]
         },
         {
             "text": "Three.js Stack Game",
             "imageSrc": "./assets/Web Development/Stack.png",
             "onClick": "https://github.com/AnkitYande/StackGame",
+            "tags": ["website", "frontend"]
         },
-    ],
-    "design": [
+        {
+            "text": "Intelligent Scissoring and Image Composition",
+            "imageSrc": "./assets/Programming Projects/imgs/iScissor.png",
+            "onClick": "./work/iScissoring.html",
+            "tags": ["cv"]
+        },
+        {
+            "text": "Remote Sensing of Crop Pathology through CV",
+            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2019.jpg",
+            "onClick": "./work/sr2019.html",
+            "tags": ["cv"]
+        },
+        {
+            "text": "Utilizing Stereoscopic Camera to assist the Visually Impaired",
+            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2018.jpg",
+            "onClick": "./work/sr2018.html",
+            "tags": ["cv"]
+        },
         {
             "text": "Design work for ReView <br> Technology Inc.",
             "imageSrc": "./assets/Design/review_logo_2.png",
             "onClick": "./design/ReView_Work.html",
+            "tags": ["design"]
         },
         {
             "text": "IxD Case Study: The Hybrid Classroom",
             "imageSrc": "./assets/Design/classroom.jpg",
             "onClick": "./design/HybridClassroom.html",
+            "tags": ["design"]
         },
         {
-            "text": "Safe Spaces <br> COVID Safety App",
+            "text": "Safe Spaces: <br> COVID Safety App Design",
             "imageSrc": "./assets/Design/safe_spaces_covid_01.png",
             "onClick": "./design/CovidApp.html",
+            "tags": ["design"]
         },
         {
             "text": "Web Landing Page Mockup",
             "imageSrc": "./assets/Design/landing_page.png",
             "onClick": "./design/Landing_Page_Mockup.html",
-        },
-    ],
-    "programs": [
-        {
-            "text": "Intelligent Scissoring and Image Composition",
-            "imageSrc": "./assets/Programming Projects/imgs/iScissor.png",
-            "onClick": "./programs/iScissoring.html",
-        },
-        {
-            "text": "Remote Sensing of Crop Pathology through CV",
-            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2019.jpg",
-            "onClick": "./programs/sr2019.html",
-        },
-        {
-            "text": "Utilizing Stereoscopic Camera to assist the Visually Impaired",
-            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2018.jpg",
-            "onClick": "./programs/sr2018.html",
+            "tags": ["design"]
         },
     ],
     "photography": [
@@ -128,54 +137,67 @@ const works = {
 
 // import {works} from './works.js'
 
-const radio = document.getElementsByName("work");
-let prev = null;
 //first time
 window.onload = () => {
-    for (var i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            prev = radio[i];
-            workHandler(radio[i].value)
-        }
+    for (let i in works["work"]) {
+        let work = works["work"][i]
+        my_works.innerHTML += formatCell(work["text"], work["imageSrc"], work["onClick"], false);
     }
 }
 //on change
-for (var i = 0; i < radio.length; i++) {
-    radio[i].addEventListener('change', function () {
-        if (this !== prev) {
-            prev = this;
-            workHandler(this.value)
-        }
-    });
+const checkboxes = document.getElementsByName("work");
+console.log("boxes:", checkboxes)
+for (var i = 0; i < checkboxes.length; i++) {
+    let checkbox = checkboxes[i]
+    checkbox.addEventListener('change', (event) => {
+        let selected = Array.prototype.slice.call(checkboxes).filter(ch => ch.checked == true).map(a => a.defaultValue);
+        filter(selected)
+    })
 }
 
-var my_works = document.querySelector(".my-works");
-var seeAll = document.querySelector(".see-all");
+const my_works = document.querySelector(".my-works");
+const seeAll = document.querySelector(".see-all");
 
-function workHandler(type) {
-
+function filter(filterList) {
     my_works.innerHTML = "";
-    seeAll.innerHTML = "";
-
-    if(works[type].length == 0){
-        my_works.innerHTML +=`<h4> Coming Soon </h4>`;
-        return;
+    let qualified = works["work"]
+    for (let i in filterList) {
+        let element = filterList[i]
+        qualified = qualified.filter(a => a["tags"].includes(element))
+        console.log(qualified)
     }
-
-    for (i in works[type]) {
-        let work = works[type][i];
-        my_works.innerHTML += formatCell(work["text"], work["imageSrc"], work["onClick"], (type == "photography" || type == "art"));
-    }
-    if (type == "photography") {
-        seeAll.innerHTML = `<a href="./gallery.html"> <h3 class="hover-underline-animation">See All 👉</h3> </a>`;
-    }
-    else if (type == "art") {
-        seeAll.innerHTML = `<a href="./art.html"> <h3 class="hover-underline-animation">See All 👉</h3> </a>`;
-    }
-    else {
-        seeAll.innerHTML = "";
+    if (qualified.length == 0) {
+        my_works.innerHTML += `<h4 style="margin-top: 2em"> No projects that match your criteria <h4> `;
+    } else {
+        for (let i in qualified) {
+            let work = qualified[i]
+            my_works.innerHTML += formatCell(work["text"], work["imageSrc"], work["onClick"], false);
+        }
     }
 }
+// function workHandler(type) {
+
+//     my_works.innerHTML = "";
+//     seeAll.innerHTML = "";
+
+//     if (works[type].length == 0) {
+//         my_works.innerHTML += `<h4> Coming Soon </h4>`;
+//         return;
+//     }
+//     for (let i in works[type]) {
+//         let work = works[type][i];
+//         my_works.innerHTML += formatCell(work["text"], work["imageSrc"], work["onClick"], (type == "photography" || type == "art"));
+//     }
+//     if (type == "photography") {
+//         seeAll.innerHTML = `<a href="./gallery.html"> <h3 class="hover-underline-animation">See All 👉</h3> </a>`;
+//     }
+//     else if (type == "art") {
+//         seeAll.innerHTML = `<a href="./art.html"> <h3 class="hover-underline-animation">See All 👉</h3> </a>`;
+//     }
+//     else {
+//         seeAll.innerHTML = "";
+//     }
+// }
 
 function formatCell(text, imageSrc, onClick, full) {
 
