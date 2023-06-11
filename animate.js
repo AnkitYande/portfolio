42628f6a3eeca597d269ad1036ae2b173cd3fdfb
Ankit Yande <ankityande@gmail.com>
2023-06-11 18:15:49 -0500

    scroll update cleanup

--- animate.js
+++ animate.js
@@ -1,148 +1,24 @@
 const nav = document.querySelector(".navbar");
 const intro = document.querySelector(".intro");
-// const imageContainer = document.querySelector(".imgContainer");
 const aloha = document.querySelector(".aloha");
 const arrow = document.querySelector(".down-arrow");
 const bio = document.querySelector(".bio");
 const fog = document.querySelector(".fog");
 const nav_left = document.getElementById("nav-left");
 
-var controller = new ScrollMagic.Controller();
-
-var imageURLs = [
-    "./AY_svg_seq/AY_svg_seq0001.svg",
-    "./AY_svg_seq/AY_svg_seq0002.svg",
-    "./AY_svg_seq/AY_svg_seq0003.svg",
-    "./AY_svg_seq/AY_svg_seq0004.svg",
-    "./AY_svg_seq/AY_svg_seq0005.svg",
-    "./AY_svg_seq/AY_svg_seq0006.svg",
-    "./AY_svg_seq/AY_svg_seq0007.svg",
-    "./AY_svg_seq/AY_svg_seq0008.svg",
-    "./AY_svg_seq/AY_svg_seq0009.svg",
-    "./AY_svg_seq/AY_svg_seq0010.svg",
-    "./AY_svg_seq/AY_svg_seq0011.svg",
-    "./AY_svg_seq/AY_svg_seq0012.svg",
-    "./AY_svg_seq/AY_svg_seq0013.svg",
-    "./AY_svg_seq/AY_svg_seq0014.svg",
-    "./AY_svg_seq/AY_svg_seq0015.svg",
-    "./AY_svg_seq/AY_svg_seq0016.svg",
-    "./AY_svg_seq/AY_svg_seq0017.svg",
-    "./AY_svg_seq/AY_svg_seq0018.svg",
-    "./AY_svg_seq/AY_svg_seq0019.svg",
-    "./AY_svg_seq/AY_svg_seq0020.svg",
-    "./AY_svg_seq/AY_svg_seq0021.svg",
-    "./AY_svg_seq/AY_svg_seq0022.svg",
-    "./AY_svg_seq/AY_svg_seq0023.svg",
-    "./AY_svg_seq/AY_svg_seq0024.svg",
-    "./AY_svg_seq/AY_svg_seq0025.svg",
-    "./AY_svg_seq/AY_svg_seq0026.svg",
-    "./AY_svg_seq/AY_svg_seq0027.svg",
-    "./AY_svg_seq/AY_svg_seq0028.svg",
-    "./AY_svg_seq/AY_svg_seq0029.svg",
-    "./AY_svg_seq/AY_svg_seq0030.svg",
-    "./AY_svg_seq/AY_svg_seq0031.svg",
-    "./AY_svg_seq/AY_svg_seq0032.svg",
-    "./AY_svg_seq/AY_svg_seq0033.svg",
-    "./AY_svg_seq/AY_svg_seq0034.svg",
-    "./AY_svg_seq/AY_svg_seq0035.svg",
-    "./AY_svg_seq/AY_svg_seq0036.svg",
-    "./AY_svg_seq/AY_svg_seq0037.svg",
-    "./AY_svg_seq/AY_svg_seq0038.svg",
-    "./AY_svg_seq/AY_svg_seq0039.svg",
-    "./AY_svg_seq/AY_svg_seq0040.svg",
-    "./AY_svg_seq/AY_svg_seq0041.svg",
-    "./AY_svg_seq/AY_svg_seq0042.svg",
-    "./AY_svg_seq/AY_svg_seq0043.svg",
-    "./AY_svg_seq/AY_svg_seq0044.svg",
-    "./AY_svg_seq/AY_svg_seq0045.svg",
-    "./AY_svg_seq/AY_svg_seq0046.svg",
-    "./AY_svg_seq/AY_svg_seq0047.svg",
-    "./AY_svg_seq/AY_svg_seq0048.svg",
-    "./AY_svg_seq/AY_svg_seq0049.svg",
-    "./AY_svg_seq/AY_svg_seq0050.svg",
-    "./AY_svg_seq/AY_svg_seq0051.svg",
-    "./AY_svg_seq/AY_svg_seq0052.svg",
-    "./AY_svg_seq/AY_svg_seq0053.svg",
-    "./AY_svg_seq/AY_svg_seq0054.svg",
-    "./AY_svg_seq/AY_svg_seq0055.svg",
-    "./AY_svg_seq/AY_svg_seq0056.svg",
-    "./AY_svg_seq/AY_svg_seq0057.svg",
-    "./AY_svg_seq/AY_svg_seq0058.svg",
-    "./AY_svg_seq/AY_svg_seq0059.svg",
-    "./AY_svg_seq/AY_svg_seq0060.svg",
-    "./AY_svg_seq/AY_svg_seq0061.svg",
-    "./AY_svg_seq/AY_svg_seq0062.svg",
-    "./AY_svg_seq/AY_svg_seq0063.svg",
-    "./AY_svg_seq/AY_svg_seq0064.svg",
-    "./AY_svg_seq/AY_svg_seq0065.svg",
-    "./AY_svg_seq/AY_svg_seq0066.svg",
-    "./AY_svg_seq/AY_svg_seq0067.svg",
-    "./AY_svg_seq/AY_svg_seq0068.svg",
-    "./AY_svg_seq/AY_svg_seq0069.svg",
-    "./AY_svg_seq/AY_svg_seq0070.svg",
-    "./AY_svg_seq/AY_svg_seq0071.svg",
-    "./AY_svg_seq/AY_svg_seq0072.svg",
-    "./AY_svg_seq/AY_svg_seq0073.svg",
-    "./AY_svg_seq/AY_svg_seq0074.svg",
-    "./AY_svg_seq/AY_svg_seq0075.svg",
-    "./AY_svg_seq/AY_svg_seq0076.svg",
-    "./AY_svg_seq/AY_svg_seq0077.svg",
-    "./AY_svg_seq/AY_svg_seq0078.svg",
-    "./AY_svg_seq/AY_svg_seq0079.svg",
-    "./AY_svg_seq/AY_svg_seq0080.svg",
-    "./AY_svg_seq/AY_svg_seq0081.svg",
-    "./AY_svg_seq/AY_svg_seq0082.svg",
-    "./AY_svg_seq/AY_svg_seq0083.svg",
-    "./AY_svg_seq/AY_svg_seq0084.svg",
-    "./AY_svg_seq/AY_svg_seq0085.svg",
-    "./AY_svg_seq/AY_svg_seq0086.svg",
-    "./AY_svg_seq/AY_svg_seq0087.svg",
-    "./AY_svg_seq/AY_svg_seq0088.svg",
-    "./AY_svg_seq/AY_svg_seq0089.svg",
-    "./AY_svg_seq/AY_svg_seq0090.svg",
-    "./AY_svg_seq/AY_svg_seq0091.svg",
-    "./AY_svg_seq/AY_svg_seq0092.svg",
-    "./AY_svg_seq/AY_svg_seq0093.svg",
-    "./AY_svg_seq/AY_svg_seq0094.svg",
-    "./AY_svg_seq/AY_svg_seq0095.svg",
-    "./AY_svg_seq/AY_svg_seq0096.svg",
-    "./AY_svg_seq/AY_svg_seq0097.svg",
-    "./AY_svg_seq/AY_svg_seq0098.svg",
-    "./AY_svg_seq/AY_svg_seq0099.svg",
-    "./AY_svg_seq/AY_svg_seq0100.svg",
-    "./AY_svg_seq/AY_svg_seq0101.svg",
-    "./AY_svg_seq/AY_svg_seq0102.svg",
-    "./AY_svg_seq/AY_svg_seq0103.svg",
-    "./AY_svg_seq/AY_svg_seq0104.svg",
-    "./AY_svg_seq/AY_svg_seq0105.svg",
-    "./AY_svg_seq/AY_svg_seq0106.svg",
-    "./AY_svg_seq/AY_svg_seq0107.svg",
-    "./AY_svg_seq/AY_svg_seq0108.svg",
-    "./AY_svg_seq/AY_svg_seq0109.svg",
-    "./AY_svg_seq/AY_svg_seq0110.svg",
-    "./AY_svg_seq/AY_svg_seq0111.svg",
-    "./AY_svg_seq/AY_svg_seq0112.svg",
-    "./AY_svg_seq/AY_svg_seq0113.svg",
-    "./AY_svg_seq/AY_svg_seq0114.svg",
-    "./AY_svg_seq/AY_svg_seq0115.svg",
-    "./AY_svg_seq/AY_svg_seq0116.svg",
-    "./AY_svg_seq/AY_svg_seq0117.svg",
-    "./AY_svg_seq/AY_svg_seq0118.svg",
-    "./AY_svg_seq/AY_svg_seq0119.svg",
-    "./AY_svg_seq/AY_svg_seq0120.svg"
-];
 
 images = []
-for (url in imageURLs) {
+for (let i = 1; i <= 120; i++) {
     let image = new Image();
-    image.src = imageURLs[url];
+    image.src = `./AY_svg_seq/AY_svg_seq${i.toString().padStart(4, '0')}.svg`;
     images.push(image);
 }
 
 // init controller
 var controller = new ScrollMagic.Controller();
 
-// build scene
+/** PLAY SLIDING ANIMATION **/ 
+
 var scene = new ScrollMagic.Scene({ triggerElement: intro, triggerHook: 0, duration: 1500 })
     // .addIndicators()
     .setPin(intro)
@@ -156,9 +32,12 @@ var scene = new ScrollMagic.Scene({ triggerElement: intro, triggerHook: 0, durat
         // nav_left.style.width = "0px";
     })
 
+// image index
 let scrollpos = 0;
-
-
+scene.on("update", e => {
+    scrollpos = e.scrollPos * 0.09;
+    scrollpos = scrollpos > 119 ? 119 : Math.round(scrollpos);
+});
 
 setInterval(() => {
     let myImg = intro.children[1];
@@ -170,6 +49,10 @@ setInterval(() => {
         arrow.style.display = scrollpos > 10 ? "none" : "block"
     }
 }, 0);
+
+
+
+/** TURN SEQ INTO NAV **/ 
 
 var tween = new TimelineMax()
     .fromTo(intro, 1, { height: '90vh' }, { height: '60px' })
@@ -187,9 +70,3 @@ var scene2 = new ScrollMagic.Scene({ triggerElement: intro, triggerHook: 0, dura
         fog.style.opacity = 0;
         // nav_left.style.width = "0px";
     });
-
-scene.on("update", e => {
-    scrollpos = e.scrollPos * 0.09;
-    scrollpos = scrollpos > 119 ? 119 : Math.round(scrollpos);
-});
-
