051bc62cf6d1a209523ddf75c4d5db5dc03ad506
Ankit Yande <ankityande@gmail.com>
2023-06-18 13:07:23 -0500

    updated work cover images to jpgs

--- load_works.js
+++ load_works.js
@@ -2,91 +2,91 @@ const works = {
     "work": [
         {
             "text": "This Portfolio!",
-            "imageSrc": "./assets/work/portfolio.png",
+            "imageSrc": "./assets/cover-imgs/portfolio.jpg",
             "onClick": "./work/MyPortfolio.html",
             "tags": ["website", "frontend", "design", "featured"]
         },
         {
             "text": "MeetApp (iOS Dev)",
-            "imageSrc": "./assets/work/meetApp.jpg",
+            "imageSrc": "./assets/cover-imgs/meetApp.jpg",
             "onClick": "./work/meetApp.html",
             "tags": ["fullstack", "frontend", "featured"]
         },
         {
             "text": "Accessibility Simulator",
-            "imageSrc": "./assets/work/accessibility.png",
+            "imageSrc": "./assets/cover-imgs/accessibility.jpg",
             "onClick": "./work/accessibility-sim.html",
             "tags": ["website", "frontend", "design", "featured"]
         },
         {
             "text": "MERN Stack Twitter Clone",
-            "imageSrc": "./assets/work/TwitterClone3.png",
+            "imageSrc": "./assets/cover-imgs/twitterClone.jpg",
             "onClick": "./work/TwitterClone.html",
             "tags": ["website", "fullstack", "featured"]
         },
         {
             "text": "Collaborative Socket Whiteboard",
-            "imageSrc": "./assets/work/draw.png",
+            "imageSrc": "./assets/cover-imgs/draw.jpg",
             "onClick": "https://github.com/AnkitYande/socket-paint-client",
             "tags": ["website", "frontend", "fullstack"]
         },
         {
             "text": "Flutter Spotify UI Clone",
-            "imageSrc": "./assets/work/SpotifyCloneMockup.png",
+            "imageSrc": "./assets/cover-imgs/spotifyClone.jpg",
             "onClick": "https://github.com/AnkitYande/SpotifyUIClone",
             "tags": ["frontend"]
         },
         {
             "text": "Interactive Spotify Player",
-            "imageSrc": "./assets/work/SpotifyPlayer2.png",
+            "imageSrc": "./assets/cover-imgs/spotifyPlayer.jpg",
             "onClick": "https://github.com/AnkitYande/ReactSpotifyPlayer",
             "tags": ["website", "frontend", "fullstack"]
         },
         {
             "text": "Three.js Stack Game",
-            "imageSrc": "./assets/work/Stack.png",
+            "imageSrc": "./assets/cover-imgs/stack.jpg",
             "onClick": "https://github.com/AnkitYande/StackGame",
             "tags": ["website", "frontend"]
         },
         {
             "text": "Intelligent Scissoring and Image Composition",
-            "imageSrc": "./assets/Programming Projects/imgs/iScissor.png",
+            "imageSrc": "./assets/cover-imgs/iScissor.jpg",
             "onClick": "./work/iScissoring.html",
             "tags": ["cv", "featured"]
         },
         {
             "text": "Remote Sensing of Crop Pathology through CV",
-            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2019.jpg",
+            "imageSrc": "./assets/cover-imgs/srPoster2019.jpg",
             "onClick": "./work/sr2019.html",
             "tags": ["cv"]
         },
         {
             "text": "Utilizing Stereoscopic Camera to assist the Visually Impaired",
-            "imageSrc": "./assets/Programming Projects/imgs/Yande Research Poster 2018.jpg",
+            "imageSrc": "./assets/cover-imgs/srPoster2018.jpg",
             "onClick": "./work/sr2018.html",
             "tags": ["cv"]
         },
         {
             "text": "Design work for ReView <br> Technology Inc.",
-            "imageSrc": "./assets/Design/review_logo_2.png",
+            "imageSrc": "./assets/cover-imgs/reviewLogo.jpg",
             "onClick": "./design/ReView_Work.html",
             "tags": ["design", "featured"]
         },
         {
             "text": "IxD Case Study: The Hybrid Classroom",
-            "imageSrc": "./assets/Design/classroom.jpg",
+            "imageSrc": "./assets/cover-imgs/classroom.jpg",
             "onClick": "./design/HybridClassroom.html",
             "tags": ["design"]
         },
         {
             "text": "Safe Spaces: <br> COVID Safety App Design",
-            "imageSrc": "./assets/Design/safe_spaces_covid_01.png",
+            "imageSrc": "./assets/cover-imgs/safeSpaces.jpg",
             "onClick": "./design/CovidApp.html",
             "tags": ["design"]
         },
         {
             "text": "Web Landing Page Mockup",
-            "imageSrc": "./assets/Design/landing_page.png",
+            "imageSrc": "./assets/cover-imgs/landingPage.jpg",
             "onClick": "./design/Landing_Page_Mockup.html",
             "tags": ["design"]
         },
